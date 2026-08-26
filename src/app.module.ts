385cfda6f9b250bc2147f018f43db0ee4e5ff5c7
nahd <iservegodidoeverythingforgod@gmail.com>
2026-08-27 00:44:57 +0700

    Inii

--- src/app.module.ts
+++ src/app.module.ts
@@ -91,6 +91,7 @@ import { RolesGuard } from './common/guards/roles.guard';
             synchronize: false,
             ssl: isSupabaseOrRemote ? { rejectUnauthorized: false } : false,
             extra: {
+              family: 4,
               max: 10,
               min: 2,
               connectionTimeoutMillis: 10000,
